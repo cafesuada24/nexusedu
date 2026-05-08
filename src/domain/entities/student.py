cd26e47524da9216bc26bd1efdd5daf2fcafb36a
cafesuada <108068915+cafesuada24@users.noreply.github.com>
2026-05-08 13:27:39 +0700

    feat(domain): remove redundant status

--- src/domain/entities/student.py
+++ src/domain/entities/student.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from uuid import UUID
 
-from src.domain.value_objects.status import InterventionStatus, RiskStatus
+from src.domain.value_objects.status import RiskStatus
 
 
 @dataclass
@@ -12,18 +12,13 @@ class Student:
     """Represents a student in the system."""
 
     sid: UUID
-    student_name: str | None
-    email: str | None
+    student_name: str
+    email: str
     major: str
     last_notified_timestamp: datetime | None
     current_risk_status: RiskStatus
-    intervention_status: InterventionStatus
-    last_notified_satisfaction: int = 0
+    last_notified_satisfaction: int | None
 
     def update_risk(self, status: RiskStatus) -> None:
         """Update the student's risk status."""
         self.current_risk_status = status
-
-    def update_intervention(self, status: InterventionStatus) -> None:
-        """Update the student's intervention status."""
-        self.intervention_status = status
