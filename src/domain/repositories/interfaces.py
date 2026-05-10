d554fc3966b4f03618cb25f184b519507711c0be
cafesuada <108068915+cafesuada24@users.noreply.github.com>
2026-05-11 01:30:13 +0700

    feat: remove ununsed alert stuff

--- src/domain/repositories/interfaces.py
+++ src/domain/repositories/interfaces.py
@@ -2,7 +2,6 @@
 
 from src.domain.repositories.activity_repository import ActivityRepository
 from src.domain.repositories.advisor_repository import AdvisorRepository
-from src.domain.repositories.alert_repository import AlertRepository
 from src.domain.repositories.email_repository import EmailRepository
 from src.domain.repositories.idempotency_repository import IdempotencyRepository
 from src.domain.repositories.metadata_repository import MetadataRepository
@@ -13,7 +12,6 @@ from src.domain.repositories.student_repository import StudentRepository
 __all__ = [
     'ActivityRepository',
     'AdvisorRepository',
-    'AlertRepository',
     'EmailRepository',
     'IdempotencyRepository',
     'MetadataRepository',
