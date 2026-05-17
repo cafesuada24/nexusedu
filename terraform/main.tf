269834d2dc1ad43a3ffc5e2ee3133c29644ee361
cafesuada <108068915+cafesuada24@users.noreply.github.com>
2026-05-17 17:46:10 +0700

    feat: enable cloud minotoring

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,18 @@ resource "google_project_service" "servicenetworking" {
   service            = "servicenetworking.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "cloudtrace" {
+  service            = "cloudtrace.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "monitoring" {
+  service            = "monitoring.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "logging" {
+  service            = "logging.googleapis.com"
+  disable_on_destroy = false
+}
